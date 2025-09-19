fa8183a69030a4aa308d0184a841a01153ff0bcc
Mikael Göransson <github@mgor.se>
2025-09-19 11:30:08 +0200

    minor changes to fork

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-kill_xvfb () {
-    local xvfb_pids=`ps aux | grep tmp/xvfb-run | grep -v grep | awk '{print $2}'`
-    if [ "$xvfb_pids" != "" ]; then
+function kill_xvfb() {
+    local xvfb_pids=$(ps aux | awk '/tmp\/xvfb-run/ {print $2}')
+    if [[ -n "$xvfb_pids" ]]; then
         echo "Killing the following xvfb processes: $xvfb_pids"
         sudo kill $xvfb_pids
-    else
-        echo "No xvfb processes to kill"
     fi
 }
 
